d93db902eceab0f6b97daf9234e14b7b25f12f25
Celine2 <celine.calmels@gmail.com>
2018-03-02 16:18:44 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+//var result=profits+savings-spendings
+//var spendings =
+
+//var profits =
+
+//var savings =
+
+//var result = (profits + saving) - spendings
+
+
+function myFunction()
+{
+	document.getElementById("personalised-msg").innerHTML = "Votre budget est positif, bravo!";
+	
+}
+
